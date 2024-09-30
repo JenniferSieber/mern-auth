6c5a9b4bc1462eaccebdbf6f05707f39a2a82b9d
Jennifer Sieber <jenwebdev2@gmail.com>
2024-09-30 09:22:14 -0600

    connect server to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
-import express from 'express'
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config();
 
-const PORT = 3000 
+const PORT = process.env.PORT || 5050;
 const app = express()
-
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log('Connected to MongoDB')
+    })
+    .catch(err => console.log(err))
+    
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}!`))
