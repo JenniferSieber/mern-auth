7e0092f2ccd2e3a8970cf2cb3e02900f72875b6d
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-01 06:43:57 -0600

    update signin UI and add cookies to signin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
+//    Signup
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -10,7 +13,30 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: "User created successfully." });
   } catch (error) {
     next(error);
-    // example using the custom errorHandler -- will use elsewhere
-    // next(errorHandler(300, "Something went wrong."))
+  }
+};
+
+//    Signin
+export const signin = async (req, res, next) => {
+  const {email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'Invalid Credentials.'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Invalid Credentials.'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // Destructure password from rest of validUser 
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+
+     // Insert token into the client side-return validUser for Profile Page
+    res
+      .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+
+  } catch (error) {
+    next(error);
   }
 };
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      Sign In
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          autoComplete="email"
+          className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          autoComplete="password"
+          className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <button 
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          { loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p className="c">Need to setup an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign Up</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">
+        {error && "Something went wrong, Try again!"}
+      </p>
     </div>
-  )
+  );
 }
