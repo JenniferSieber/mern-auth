d5bc2c6313cd5ea3f3f7ae0c9513ec3a64780cd5
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-02 09:36:58 -0600

    test

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-
+// test
 export default function PrivateRoute() {
     const { currentUser } = useSelector(state => state.user);
     
