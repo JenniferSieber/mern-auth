6b8e3bffcec7efcb08fecbf281eb9a1b2272478c
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-02 09:26:52 -0600

    create private route for profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import PrivateRoute from "./components/PrivateRoute";
 import About from './pages/About';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
-import SignOut from './pages/SignOut';
 import SignUp from './pages/SignUp';
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+  <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/sign-out" element={<SignOut />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
     </Routes>
-
-  </BrowserRouter>;
+  </BrowserRouter>
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom'
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function Header() {
+  const { currentUser } = useSelector(state => state.user);
+
   return (
     <header className="bg-slate-200">
-
       <nav className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold">Auth App</h1>
         </Link>
-        
+
         <ul className="flex gap-4">
           <Link to="/">
             <li>Home</li>
@@ -16,12 +18,15 @@ export default function Header() {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/sign-in">
-            <li>Sign In</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt="profile" className="h-7 w-7 rounded-full object-cover" />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </nav>
-
     </header>
-  )
+  );
 }
